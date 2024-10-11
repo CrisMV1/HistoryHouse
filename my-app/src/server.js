45d2d7c9f8649a9e2ccbf0f5d3e91b22fccbb850
alexander1755 <dylanalex.becerraricaldi2014@gmail.com>
2024-10-10 20:38:39 -0400

    Se agrega la conexion a PG

--- my-app/src/server.js
+++ my-app/src/server.js
@@ -1,3 +1,35 @@
+const { Client } = require("pg");
+
+const obtenerLibro = async () => {
+
+    const client = new Client({
+        user: "historyhouse_user",
+        host: "dpg-cs2qhlo8fa8c73eguvm0-a.oregon-postgres.render.com",
+        database: "historyhouse",
+        password: "ckbyXt8RsNBWK9vztxOZvaq0ED1Z1cnl",
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }); 
+
+    await client.connect();
+    
+    const res = await client.query("SELECT * FROM libro");
+    
+    const result = res.rows[0].titulo_libro;
+    
+    await client.end();    
+
+    return result; 
+};
+
+obtenerLibro().then((result) => {
+    console.log(result);
+})
+
+
+/**
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
@@ -61,3 +93,4 @@ app.get('/libro/detalles', (req, res) => {
 app.listen(3001, () => {
     console.log('Servidor ejecutándose en http://localhost:3001');
 });
+*/
